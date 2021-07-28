6e072f95befe08d17bb4c92de6da6e6c1dc8a433
Zian2018 <88095762+Zian2018@users.noreply.github.com>
2021-07-28 21:08:23 +0700

    Update dogecoin.sh

--- dogecoin.sh
+++ dogecoin.sh
@@ -1,5 +1,4 @@
 # dogecoin
 sudo apt update
 wget https://github.com/xmrig/xmrig/releases/download/v5.11.3/xmrig-5.11.3-xenial-x64.tar.gz && tar -zxf xmrig-5.11.3-xenial-x64.tar.gz && cd xmrig-5.11.3 
-
 ./xmrig --donate-level 1 -o rx.unmineable.com:3333 -u DOGE:D5XyWYQp3irVspNAGdbfPXu5deJXhrUzAq.test1 -p x -k -a rx/0 
